accf32f4ff5655b44861e5c6b3578926398eaf68
khezen <simonneaug@gmail.com>
2018-03-04 02:00:58 +0100

    errorhandling

--- errors/http.go
+++ errors/http.go
@@ -10,3 +10,21 @@ var (
 	// ErrWrongMethod - 405
 	ErrWrongMethod = errors.New("ErrWrongMethod - The request http method does not match expectation")
 )
+
+// HTTPErr -
+type HTTPErr struct {
+	error
+	StatusCode int
+}
+
+// HTTPStatusCode -
+func HTTPStatusCode(err error) int {
+	switch err {
+	case ErrPathNotFound:
+		return 404
+	case ErrWrongMethod:
+		return 405
+	default:
+		return 500
+	}
+}
--- server/errors.go
+++ server/errors.go
@@ -9,19 +9,11 @@ import (
 )
 
 func (s *Server) serveError(w http.ResponseWriter, r *http.Request, err error) {
+	fmt.Printf("%v", err.Error())
 	w.Header().Set("Connection", "close")
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	switch err {
-	case errors.ErrPathNotFound:
-		w.WriteHeader(http.StatusNotFound)
-		io.WriteString(w, err.Error())
-		return
-	case errors.ErrWrongMethod:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		io.WriteString(w, err.Error())
-		return
-	}
-	fmt.Printf("%v", err.Error())
+	statusCode := errors.HTTPStatusCode(err)
+	w.WriteHeader(statusCode)
 	w.WriteHeader(http.StatusInternalServerError)
 	io.WriteString(w, err.Error())
 }
